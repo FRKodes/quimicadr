33fb0df097ca79b26d65122d760c27a13f4d6e80
frkodes <frkalderon@gmail.com>
2015-03-02 15:38:14 -0600

    Setting up envvar on scripts

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -3,7 +3,7 @@
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="slick/slick.min.js"></script>
-@if($_ENV['APP_ENV'] != 'production')
+@if($_ENV['APP_ENV'] == 'production')
 	<script type="text/javascript" src="{{ elixir('js/custom.min.js')}}"></script>
 @else
 	<script type="text/javascript" src="js/custom.js"></script>
