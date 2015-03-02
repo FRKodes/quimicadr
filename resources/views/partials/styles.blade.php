4d1f7afbc63f230ba9581a65b20e50d869fc5e66
frkodes <frkalderon@gmail.com>
2015-03-02 15:36:13 -0600

    Fix http or https call on google fonts

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -1,4 +1,4 @@
-<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic' rel='stylesheet' type='text/css'>
 @if($_ENV['APP_ENV'] == 'production')
 	<link rel="stylesheet" href="{{ elixir('css/all.min.css')}}">
 	<link rel="stylesheet" href="slick/slick-all.css">
