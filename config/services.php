495f82b2131c412c164138fa7773b31f6a171dbb
frkodes <frkalderon@gmail.com>
2016-04-26 18:54:22 -0500

    Changing sending emails to mailgun

--- config/services.php
+++ config/services.php
@@ -16,6 +16,11 @@ return [
 
 	'mandrill' => [
 		'secret' => $_ENV['MANDRILL_SECRET']
+	],
+
+	'mailgun' => [
+	    'domain' => $_ENV['MAILGUN_DOMAIN'],
+	    'secret' => $_ENV['MAILGUN_KEY'],
 	]
 
 ];
